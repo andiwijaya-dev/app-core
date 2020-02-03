903f1d7fcd7b37a90dcd7bf7927974d1fa5b0740
andyv4 <andyv4@gmail.com>
2020-02-03 12:02:15 +0700

    v1.0.025

--- src/views/components/chat-message-item.blade.php
+++ src/views/components/chat-message-item.blade.php
@@ -3,7 +3,7 @@
   @if(isset($item->extra['images'][0]))
   <div class="pad-1">
     @foreach($item->extra['images'] as $image)
-      <span class="img unloaded can-preview" data-src="/images/{{ $image }}" style="width:2.5em;height:2.5em"></span>
+      <span class="img unloaded can-preview" data-src="/images/{{ $image }}" style="width:3em;height:3em"></span>
     @endforeach
   </div>
   @endif
